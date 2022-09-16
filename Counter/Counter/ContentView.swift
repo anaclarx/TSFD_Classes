16609b5601de1a680b966c55459b8882a3e2ea6a
Ana Clara Granato <anaclaragranato@gmail.com>
2022-09-16 16:06:00 +0200

    copying a personal project of counter in swift to create my mileStones ad commits

---
+++ Counter/Counter/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Ana Clara Filgueiras Granato on 08/08/22.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State var counter = 0
+    var body: some View {
+        HStack(alignment: .center){
+            Button( "-"){
+                self.counter -= 1
+            }
+            Text(String(counter))
+                .padding()
+            Button( "+"){
+                self.counter += 1
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Ana Clara Filgueiras Granato on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
