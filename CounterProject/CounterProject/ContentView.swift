2a32da654c3ed3c82dbf2fd0e140758002790ea4
Ana Clara Granato <anaclaragranato@gmail.com>
2022-09-17 15:11:55 +0200

    adding logo to view and resizing to fit as wished

--- CounterProject/CounterProject/ContentView.swift
+++ CounterProject/CounterProject/ContentView.swift
@@ -11,17 +11,22 @@ import SwiftUI
 struct ContentView: View {
     @State var counter = 0
     var body: some View {
-        HStack(alignment: .center){
-            Button( "-"){
-                self.counter -= 1
+        VStack(alignment: .center){
+            Image("LogoCounter")
+                .resizable()
+                .frame(width: 400.0, height: 200.0)
+            HStack(alignment: .center){
+                Button( "-"){
+                    self.counter -= 1
+                }
+                .foregroundColor(.red)
+                Text(String(counter))
+                    .padding()
+                Button( "+"){
+                    self.counter += 1
+                }
+                .foregroundColor(.green)
             }
-            .foregroundColor(.red)
-            Text(String(counter))
-                .padding()
-            Button( "+"){
-                self.counter += 1
-            }
-            .foregroundColor(.green)
         }
     }
 }
