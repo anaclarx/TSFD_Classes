75bc842727ff595390b4f0ebde36c08364876acd
Ana Clara Granato <anaclaragranato@gmail.com>
2022-09-17 14:46:28 +0200

    initializing new xcode counter project - previous was broken

---
+++ CounterProject/CounterProject/CounterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterProjectApp.swift
+//  CounterProject
+//
+//  Created by Ana Clara Filgueiras Granato on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CounterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
